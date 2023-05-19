c0b5df58df74119dceb760ec9752acde755fba77
jbcol <jeanbaptistecolj@gmail.com>
2023-05-19 15:27:19 +0200

    blockshell

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://github.io/Passhield',
+  site: 'https://blockshell.github.io',
   base: '/frontend',
 });
